e7f1eafd3e44305250a4bf6988f2178390d466cf
Marcos Felipe <p.marcosfelipe@gmail.com>
2025-06-04 05:57:59 +0100

    create calculator two

---
+++ src/calculators/calculator_two.py
@@ -0,0 +1,41 @@
+from typing import Dict, List
+from flask import request as FlaskRequest
+from src.drivers.numpy_handler import NumpyHandler
+
+
+class CalculatorTwo:
+    """
+    * N numbers are sent;
+    * Process 1: All N numbers are multiplied by 11 and raised to the power 0.95;
+    * Process 2: The Standard Deviation is calculated of the results and returned the inverse (1 / result);
+    * Tip: Use Numpy to calculate the Standard Deviation.
+    """
+
+    def calculate(self, request: FlaskRequest) -> Dict:  # type: ignore
+        body = request.json
+        print(body)
+        data = self.__validate_body(body)
+        print(data)
+
+        calculated_data = self.__process_data(data)
+
+        formatted_response = self.__format_response(calculated_data)
+        return formatted_response
+
+    def __validate_body(self, body: Dict) -> List[float]:
+        if "numbers" not in body:
+            raise Exception("Body format is not correct!")
+
+        data = body["numbers"]
+        return data
+
+    def __process_data(self, data: List[float]) -> float:
+        numpy_handler = NumpyHandler()
+        first_process_result = [(num * 11) ** 0.96 for num in data]
+
+        result = numpy_handler.standard_deviation(first_process_result)
+
+        return 1 / result
+
+    def __format_response(self, calculated_data: float) -> Dict:
+        return {"data": {"Calculator": 2, "result": round(calculated_data, 2)}}
---
+++ src/calculators/calculator_two_test.py
@@ -0,0 +1,20 @@
+from typing import Dict
+from pytest import raises
+from .calculator_two import CalculatorTwo
+
+
+class MockRequest:
+    def __init__(self, body: Dict) -> None:
+        self.json = body
+
+
+def test_calculate():
+    mock_request = MockRequest({"numbers": [2.12, 4.62, 1.32]})
+    calculator_two = CalculatorTwo()
+
+    formatted_response = calculator_two.calculate(mock_request)
+    print()
+    print(formatted_response)
+
+    assert isinstance(formatted_response, dict)
+    assert formatted_response == {"data": {"Calculator": 2, "result": 0.08}}
